78808291fe03694cbe5751cf2f5e2f16039381b8
Ruud Andriessen <ruud.andriessen@gmail.com>
2017-09-17 11:59:49 +0200

    Added basic search input

--- app/views/Search/Search.tsx
+++ app/views/Search/Search.tsx
@@ -2,15 +2,29 @@
 
 // Libraries
 import * as React        from 'react';
-import styledComponents  from 'styled-components';
+import style  from 'styled-components';
 
+const SearchContainer = style.div`
+    padding: 15px;
+`;
+
+const SearchInput = style.input`
+    width: calc(100% - 20px);
+    height: 30px;
+    padding: 10px;
+    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+    border: none;
+    :focus {
+        outline: none;
+    }
+`;
 
 class Search extends React.Component<{}, {}> {
     render() {
         return (
-            <div>
-                search!
-            </div>
+            <SearchContainer>
+                <SearchInput placeholder="Search..."/>
+            </SearchContainer>
         );
     }
 }
