aa8979266f053fa16977f6f239c02aa200155bf7
Ruud Andriessen <ruud.andriessen@gmail.com>
2017-10-04 12:13:59 +0200

    write to file

--- src/fetch_scholar_data.py
+++ src/fetch_scholar_data.py
@@ -3,26 +3,28 @@ from models.Data import Data
 import time
 
 
+text_file = open("scholar.txt", "w")
+
 def fetch_citations():
-    return
     for key, paper in Data.papers.items():
         time.sleep(0.5)
-        fetch_paper(paper.title)
+        fetch_paper(paper)
+    text_file.close()
 
 
-def fetch_paper(paper_title):
+def fetch_paper(paper):
     querier = scholar.ScholarQuerier()
     settings = scholar.ScholarSettings()
     querier.apply_settings(settings)
 
     query = scholar.SearchScholarQuery()
-    query.set_phrase(paper_title)
+    query.set_phrase(paper.title)
     query.set_scope(True)
     query.set_num_page_results(1)
 
     querier.send_query(query)
 
-    print(paper_title)
-    print(len(querier.articles))
     if len(querier.articles) > 0:
-        print(querier.articles[0]['num_citations'])
+        num_citations = querier.articles[0]['num_citations']
+        print(paper.id + "" + num_citations)
+        text_file.write("%s %s" % (paper.id, num_citations))
